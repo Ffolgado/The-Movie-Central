962b3431a4c7c9c5f6c88bda0341409face7ae44
Fatima <104076002+Ffolgado@users.noreply.github.com>
2024-08-08 13:48:48 +0200

    Update lighthouserc.js

--- github/workflows/lighthouserc.js
+++ github/workflows/lighthouserc.js
@@ -1,16 +1,8 @@
-module.exports = {
-  ci: {
-    collect: {
-      staticDistDir: './dist', // Cambia esto a la ruta de tu directorio de distribución
-      url: ['http://localhost'], // URL a analizar
-    },
-    assert: {
-      assertions: {
-        'categories:performance': ['warn', { minScore: 0.8 }],
-      },
-    },
-    upload: {
-      target: 'temporary-public-storage',
-    },
-  },
-};
+{
+  "ci": {
+    "collect": {
+      "staticDistDir": "path/to/your/static/files"
+    }
+  }
+}
+
